4d92e7faec09812791751fef2131655e071c6a0b
Fernando Lopez <lopezfer@globalhitss.com>
2020-01-15 20:46:05 -0300

    fix: cambio en schema

--- controllers/v2/schema.js
+++ controllers/v2/schema.js
@@ -1,11 +1,11 @@
 const graphql = require('graphql');
-const { Hotel, Room } = require('../../models/Hotel');
+const { Hotel } = require('../../models/Hotel');
 
 const { 
     GraphQLObjectType, 
     GraphQLString, 
     GraphQLID, 
-    GraphQLInt,
+    GraphQLFloat,
     GraphQLSchema, 
     GraphQLList 
 } = graphql;
@@ -16,57 +16,18 @@ const HotelType = new GraphQLObjectType({
         id: { type: GraphQLID  },
         name: { type: GraphQLString }, 
         description: { type: GraphQLString },
-        rooms: {
-        type: RoomType,
-        resolve(parent, args) {
-            return Room.findById(parent.id);
-        }
-    }
+        image: { type: GraphQLString },
+        stars: { type: GraphQLFloat }
     })
 });
-
-const RoomType = new GraphQLObjectType({
-    name: 'Rooms',
-    fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        image: { type: GraphQLInt },
-        hotel:{
-            type: new GraphQLList(HotelType),
-            resolve(parent,args){
-                return Hotel.find({ id: parent.id });
-            }
-        }
-    })
-})
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        hotel: {
-            type: HotelType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args) {
-                return Hotel.findById(args.id);
-            }
-        },
         hoteles:{
             type: new GraphQLList(HotelType),
             resolve(parent, args) {
                 return Hotel.find({});
-            }
-        },
-        room:{
-            type: RoomType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args) {
-                return Room.findById(args.id);
-            }
-        },
-        rooms:{
-            type: new GraphQLList(RoomType),
-            resolve(parent, args) {
-                return Room.find({});
             }
         }
     }
