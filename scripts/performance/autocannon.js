83c49e3120b7d3670a35d31f8f66ecee69c476cc
Fernando Lopez <lopezfer@globalhitss.com>
2020-01-20 16:44:46 -0300

    fix: fix script

--- scripts/performance/autocannon.js
+++ scripts/performance/autocannon.js
@@ -1,8 +1,8 @@
 const autocannon = require('autocannon');
-const PORT = process.env.PORT || '5000';
+const config = require('../../config/index');
 
 autocannon({
-    url: `http://localhost:${PORT}/api/hoteles`,
+    url: `http://localhost:${config.port}/api/v1/hoteles`,
     connections: 20,
     pipelining: 1,
     duration: 10 
