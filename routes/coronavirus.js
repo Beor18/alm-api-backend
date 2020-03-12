425209fa263f7fc20745c733e9d9228db2888f40
Fernando Lopez <lopezfer@globalhitss.com>
2020-03-12 17:01:27 -0300

    feat: actualizado endpoint

--- routes/coronavirus.js
+++ routes/coronavirus.js
@@ -6,7 +6,7 @@ const coronavirusController = require('../controllers/v1/coronavirus.controller'
 // Ruta mostrar todos los hoteles y agregar hotel
 router.route('/coronavirus')
     .get(coronavirusController.getNoticia)
-    // .post(coronavirusController.postNoticia)
+    .post(coronavirusController.postNoticia)
     .put(coronavirusController.modificarNoticia)
 
 router.route('/coronavirus/borrar')
