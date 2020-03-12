41a883c0c4d198eadeec8d04f53cb3f5ff2b9259
Fernando Lopez <lopezfer@globalhitss.com>
2020-03-12 12:58:57 -0300

    feat: actualizado endpoint

--- controllers/v1/coronavirus.controller.js
+++ controllers/v1/coronavirus.controller.js
@@ -7,7 +7,7 @@ async function getNoticia(req, res, next) {
     try {
         await Coronavirus
             .find({})
-            .populate('rooms')
+            .populate('noticias')
             .exec((err, data) => {
                 Coronavirus.countDocuments((err, count) => {
                     if (err) return next(err);
