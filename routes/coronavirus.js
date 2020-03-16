a19e2e20776b6409f38ed96e65595b37ddae55b4
Fernando Lopez <lopezfer@globalhitss.com>
2020-03-16 16:36:20 -0300

    feat: cache

--- routes/coronavirus.js
+++ routes/coronavirus.js
@@ -7,12 +7,12 @@ const coronavirusController = require('../controllers/v1/coronavirus.controller'
 // Ruta mostrar todos los hoteles y agregar hotel
 router.route('/coronavirus')
     .get(coronavirusController.getNoticia, cache.get, cache.set)
-    .post(coronavirusController.postNoticia)
+    //.post(coronavirusController.postNoticia)
 
 router.route('/coronavirus/:id')
     .put(coronavirusController.modificarNoticia, cache.clear)
 
-router.route('/coronavirus/borrar')
-    .post(coronavirusController.deleteAllNoticia);
+// router.route('/coronavirus/borrar')
+//     .post(coronavirusController.deleteAllNoticia);
 
 module.exports = router;
